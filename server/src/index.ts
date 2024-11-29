54689103e53d7b49ecbfb246bdd7fe117582f373
lsj2015-hub <lsj2015@hotmail.com>
2024-11-29 22:24:46 +0800

    server modifications

--- server/src/index.ts
+++ server/src/index.ts
@@ -33,7 +33,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/teams', teamRoutes);
 
 /* SERVER */
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+const PORT = Number(process.env.PORT) || 4000;
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
